1554805baf0fc180d9330a8367d35ca6769573d6
iztok <iztok.kucan@gmail.com>
2025-11-04 23:52:33 +0000

    ...

--- src/module/long_running_test.py
+++ src/module/long_running_test.py
@@ -5,11 +5,12 @@ import time
 
 
 def xyz(**kwargs):
-    print("YEP. RUNNING entry point __main__")
 
     conda_env = os.environ.get("CONDA_DEFAULT_ENV")
+    print("------------------------")
     print(f"Current conda environment: {conda_env}")
     print(f"Python environment path: {sys.prefix}")
+    print("------------------------")
 
     while 1 < 2:
         print(f"{dt.datetime.now().strftime('%Y%m%d-%H:%M:%S')} : running....")
